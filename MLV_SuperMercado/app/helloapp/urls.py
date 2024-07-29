63f5ad8edbae7595c538984a7713d61fb6fddb99
Leo Gonçalves <leonardo.flora@estudante.ufla.br>
2024-07-29 17:13:56 -0300

    Tela Inicial e Tela de Login

--- MLV_SuperMercado/app/helloapp/urls.py
+++ MLV_SuperMercado/app/helloapp/urls.py
@@ -3,4 +3,5 @@ from helloapp import views
 
 urlpatterns = [
     path('', views.hello_world, name='hello_world'),
+    path('login/', views.login_view, name='login'),
 ]
--- MLV_SuperMercado/app/helloapp/views.py
+++ MLV_SuperMercado/app/helloapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def hello_world(request):
     return render(request, 'helloapp/hello_world.html', {})
+
+def login_view(request):
+    return render(request, 'helloapp/login.html')
